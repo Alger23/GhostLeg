8bae75b57f5245bf040566ec5f1bbcd4b050870a
Alger Chen <alger.chen23@gmail.com>
2017-12-31 21:51:23 +0800

    Add start-text and end-text, Add onClick handler

--- src/app/models/Leg.ts
+++ src/app/models/Leg.ts
@@ -9,6 +9,9 @@ export class Leg {
   container: createjs.Container;
   shapeCommands: { [id: string]: any } = {};
   bounds: { x: number; y: number; w: number; h: number };
+
+  onClick: (Leg) => void;
+
   constructor(id: any) {
     this.id = id;
     this.bounds = { x: 0, y: 0, w: 10, h: 10 };
@@ -29,7 +32,29 @@ export class Leg {
     let rect = container.addChild(new createjs.Shape());
     this.shapeCommands['rect'] = rect.graphics.beginFill('lightgreen').drawRect(-15, -10, 30, 20).command;
 
+    let endText = container.addChild(new createjs.Text(this.endText, '12px Arial', '#111'));
+    endText.lineWidth = 50;
+    endText.lineHeight = 22;
+    endText.textBaseline = 'top';
+    endText.textAlign = 'center';
+    this.shapeCommands['endText'] = endText;
+
+    let startText = container.addChild(new createjs.Text(this.startText, '12px Arial', '#111'));
+    startText.lineWidth = 50;
+    startText.lineHeight = 22;
+    startText.textBaseline = 'top';
+    startText.textAlign = 'center';
+    this.shapeCommands['startText'] = startText;
+
     stage.addChild(container);
+
+
+    circle.addEventListener('click', () => {
+      if (this.onClick) {
+        this.onClick(this);
+      }
+    });
+
   }
 
   removeShapes(stage: createjs.Stage) {
@@ -60,5 +85,13 @@ export class Leg {
     let rectCmd = this.shapeCommands['rect'];
     rectCmd.x = this.bounds.x - (rectCmd.w / 2);
     rectCmd.y = this.bounds.y;
+
+    let endText = this.shapeCommands['endText'];
+    endText.x = this.bounds.x;
+    endText.y = this.bounds.y;
+
+    let startText = this.shapeCommands['startText'];
+    startText.x = this.bounds.x;
+    startText.y = this.bounds.y + this.bounds.h;
   }
 }

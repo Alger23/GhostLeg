a277236af7935f6ce3362e89178cea4eb86cc1a2
Alger Chen <alger.chen23@gmail.com>
2017-12-31 16:22:02 +0800

    Add createjs.Stage element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,39 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, AfterViewInit } from '@angular/core';
+import * as createjs from 'createjs-module';
+import { create } from 'domain';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   canvasId = 'canvas';
   canvas: HTMLCanvasElement;
   size: { w: number; h: number };
+
+  stage: createjs.Stage;
+
   constructor() {}
 
   ngOnInit() {
     this.canvas = <HTMLCanvasElement>document.getElementById(this.canvasId);
     this.resize();
+  }
+
+  ngAfterViewInit(): void {
+    this.stage = new createjs.Stage(this.canvasId);
+    let stage = this.stage;
+
+    let shape = stage.addChild(new createjs.Shape());
+    shape.graphics
+      .setStrokeStyle(1)
+      .beginStroke('red')
+      .drawCircle(50, 50, 40);
+    // stage.update();
+
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener('tick', stage);
   }
 
   @HostListener('window:resize')

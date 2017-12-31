5d94f88f63802962aa3b1119c1d326cdef7accf7
Alger Chen <alger.chen23@gmail.com>
2017-12-31 15:10:56 +0800

    Add canvas element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  canvasId = 'canvas';
+  canvas: HTMLCanvasElement;
+  size: { w: number; h: number };
+  constructor() {}
+
+  ngOnInit() {
+    this.canvas = <HTMLCanvasElement>document.getElementById(this.canvasId);
+    this.resize();
+  }
+
+  @HostListener('window:resize')
+  resize() {
+    this.size = {
+      w: window.innerWidth - 20,
+      h: window.innerHeight - 50
+    };
+    this.canvas.width = this.size.w;
+    this.canvas.height = this.size.h;
+  }
 }
